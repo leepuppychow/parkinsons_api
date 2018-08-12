751ec1862186de82dedf525c913dae246a5b2e60
leepuppychow <leepuppychow@gmail.com>
2018-08-12 00:36:03 -0600

    Switching to model-controller type structure with RESTful conventions

---
+++ controllers/patients.go
@@ -0,0 +1,29 @@
+package patients
+
+import (
+	"net/http"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"parkinsons/models"
+
+	"github.com/gorilla/mux"
+)
+
+func ToJSON(arg interface{}) []byte {
+	json, err := json.MarshalIndent(arg, "", "   ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	return json
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	w.Write(ToJSON(patient.All()))
+}
+
+func Show(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	w.Write(ToJSON(patient.Find(id)))
+}
--- main.go
+++ main.go
@@ -3,67 +3,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"parkinsons/models"
-	"path"
-	"strconv"
-	"strings"
+	"parkinsons/routes"
 )
 
-func ConvertToJSON(arg interface{}) []byte {
-	json, err := json.MarshalIndent(arg, "", "   ")
-	if err != nil {
-		fmt.Println(err)
-	}
-	return json
-}
-
-func CheckForIDInRoute(route string) (bool, int) {
-	i, err := strconv.Atoi(route)
-	if err != nil {
-		return false, 0
-	}
-	return true, i
-}
-
-func routes(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	route := r.URL.Path
-	method := r.Method
-
-	switch true {
-	case route == "/":
-		fmt.Fprintf(w, "Welcome to Parkinson's API built in GO")
-	case strings.HasPrefix(route, "/api/v1/patients") && method == "GET":
-		var response []byte
-		found, id := CheckForIDInRoute(path.Base(r.URL.Path))
-		if !found {
-			response = ConvertToJSON(patients.GetAllPatients())
-		} else {
-			patient := patients.GetOnePatient(id)
-			if patient.Id == 0 {
-				response = ConvertToJSON("Patient not found")
-			} else {
-				response = ConvertToJSON(patients.GetOnePatient(id))
-			}
-		}
-		w.Write(response)
-	case strings.HasPrefix(route, "/api/v1/users"):
-		response := ConvertToJSON("Get all users")
-		w.Write(response)
-	case strings.HasPrefix(route, "/api/v1/providers"):
-		response := ConvertToJSON("Get all users")
-		w.Write(response)
-	default:
-		fmt.Fprintf(w, "Path not found")
-	}
-}
-
 func main() {
-	http.HandleFunc("/", routes)
+	router := routes.NewRouter()
+
 	fmt.Println("Server running on port 8000")
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
 }
